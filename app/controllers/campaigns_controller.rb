ff18d72aac40305138a26aced463eecb445ac9c2
Christine Movius <christine.movius@gmail.com>
2016-03-20 12:37:33 -0400

    Create CampaignsController.

---
+++ app/controllers/campaigns_controller.rb
@@ -0,0 +1,43 @@
+class CampaignsController < ApplicationController
+  def index
+    @campaign = Campaign.all.order(:id)
+  end
+
+  def show
+    @campaign = Campaign.find(params[:id])
+  end
+
+  def new
+    @campaign = Campaign.new
+  end
+
+  def create
+    @campaign = Campaign.create! campaign_params
+
+    redirect_to campaign_path(@campaign)
+  end
+
+  def edit
+    @campaign = Campaign.find(params[:id])
+  end
+
+  def update
+    @campaign = Campaign.find(params[:id])
+    @campaign.update campaign_params
+
+    redirect_to campaign_path(@campaign)
+  end
+
+  def destroy
+    @campaign = Campaign.find(params[:id])
+    @campaign.destroy
+
+    redirect_to campaigns_path
+  end
+
+  private
+  def campaign_params
+    params.require(:campaign).permit(:name, :summary)
+  end
+
+end
