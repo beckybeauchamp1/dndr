48eb1592a6883f5fd0a84714e77d815f8581ce7a
Christine Movius <christine.movius@gmail.com>
2016-03-19 20:55:08 -0400

    Create migration files for campaign, character, and dmaster. Have not actually migrated yet though.

---
+++ db/migrate/20160320001502_create_campaigns.rb
@@ -0,0 +1,15 @@
+class CreateCampaigns < ActiveRecord::Migration
+  def change
+    create_table :campaigns do |t|
+      t.string :name
+      t.text :summary
+      t.datetime :updated_at
+    end
+
+    create_table :dmasters do |t|
+      t.string :name
+      t.belongs_to :campaign, index: true, foreign_key: true
+      t.datetime :updated_at
+    end
+  end
+end
---
+++ db/migrate/20160320001934_create_characters.rb
@@ -0,0 +1,31 @@
+class CreateCharacters < ActiveRecord::Migration
+  def change
+    create_table :characters do |t|
+      t.string :player_name
+      t.string :char_name
+      t.string :class
+      t.integer :level
+      t.string :race
+      t.string :background
+      t.string :alignment
+      t.string :archetype
+      t.integer :xp
+      t.integer :hp_max
+      t.integer :hp_current
+      t.integer :armor_class
+      t.integer :initiative
+      t.integer :speed
+      t.integer :prof_bonus
+      t.integer :inspiration
+      t.text :languages
+      t.text :features
+      t.text :equipment
+      t.text :personality
+      t.text :ideals
+      t.text :bonds
+      t.text :flaws
+      t.datetime :updated_at
+      t.belongs_to :campaign, index: true, foreign_key: true
+    end
+  end
+end
