507bc583f6f7ad93d8c78d7f19e33a05168a075e
Christine Movius <christine.movius@gmail.com>
2016-03-20 00:12:42 -0400

    Create 10 characters that each correlate to a campaign.

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,8 +11,35 @@ Campaign.destroy_all
 
 10.times do |index|
   Campaign.create!(name: Faker::Company.name,
-                summary: Faker::Company.catch_phrase)
-  Dmaster.create!(name: Faker::Name.name, campaign_id: index+1)
+                  summary: Faker::Company.catch_phrase)
+  Dmaster.create!(name: Faker::Name.name,
+                  campaign_id: index+1)
+  Character.create!(
+    player_name: Faker::Name.name,
+    char_name: Faker::Superhero.name,
+    klass: Faker::Company.profession,
+    level: 1,
+    race: Faker::StarWars.specie,
+    background: Faker::StarWars.planet,
+    alignment: "Chaotic Good",
+    archetype: nil,
+    xp: 0,
+    hp_max: Faker::Number.between(1, 12),
+    hp_current: 12,
+    armor_class: 16,
+    initiative: 2,
+    speed: 30,
+    prof_bonus: 2,
+    inspiration: 0,
+    languages: nil,
+    features: nil,
+    equipment: nil,
+    personality: nil,
+    ideals: nil,
+    bonds: nil,
+    flaws: nil,
+    campaign_id: index+1
+  )
 end
 
-p "Created #{Campaign.count} campaigns and #{Dmaster.count} dungeon masters."
+p "Created #{Campaign.count} campaigns, #{Dmaster.count} dungeon masters, and #{Character.count} characters."
