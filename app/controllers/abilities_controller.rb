1894e63c8abde0dc608b8c0a532792ad2f320e54
Christine Movius <christine.movius@gmail.com>
2016-03-23 15:18:54 -0400

    Create AbilitiesController.

---
+++ app/controllers/abilities_controller.rb
@@ -0,0 +1,52 @@
+class AbilitiesController < ApplicationController
+  def show
+    @campaign = Campaign.find(params[:campaign_id])
+    @character = @campaign.characters.find(params[:character_id])
+    @ability = @character.ability
+  end
+
+  def new
+    @campaign = Campaign.find(params[:campaign_id])
+    @character = @campaign.characters.find(params[:character_id])
+    @ability = Ability.create!
+    @character.ability = @ability
+  end
+
+  def create
+    @campaign = Campaign.find(params[:campaign_id])
+    @character = @campaign.characters.find(params[:character_id])
+    @ability = @character.ability.create! ability_params
+
+    redirect_to campaign_path(@character)
+  end
+
+  def edit
+    @campaign = Campaign.find(params[:campaign_id])
+    @character = @campaign.characters.find(params[:character_id])
+    @ability = @character.ability
+  end
+
+  def update
+    @campaign = Campaign.find(params[:campaign_id])
+    @character = @campaign.characters.find(params[:character_id])
+    @ability = @character.ability
+    @ability.update ability_params
+
+    redirect_to campaign_path(@character)
+  end
+
+  def destroy
+    @campaign = Campaign.find(params[:campaign_id])
+    @character = @campaign.characters.find(params[:character_id])
+    @ability = @character.ability
+    @ability.destroy
+
+    redirect_to campaign_path(@character)
+  end
+
+  private
+  def ability_params
+    params.require(:ability).permit(:strength, :strength_mod, :dexterity, :dexterity_mod, :constitution, :constitution_mod, :intelligence, :intelligence_mod, :wisdom, :wisdom_mod, :charisma, :charisma_mod)
+  end
+
+end
