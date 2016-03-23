1074827e6f870200e6aaeb033f0cf67d3c47fc8d
Christine Movius <christine.movius@gmail.com>
2016-03-23 15:01:12 -0400

    Add ability nested routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root to: 'campaigns#index'
   resources :campaigns do
-    resources :characters
+    resources :characters do
+      resource :ability
+    end
     resource :dmaster
     resources :adventures
   end
